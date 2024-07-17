2569a57b0291a4610ce4e92136287fb3e3ac5441
Miku172002 <girijashankardas690@gmail.com>
2024-07-17 22:06:02 +0530

    2nd

---
+++ Minor_Project/target/debug/deps/Minor_Project.d
@@ -0,0 +1,5 @@
+C:\Users\girij\hello_rust\Minor_Project\target\debug\deps\Minor_Project.exe: src/main.rs
+
+C:\Users\girij\hello_rust\Minor_Project\target\debug\deps\Minor_Project.d: src/main.rs
+
+src/main.rs:
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let num1=10;
+    let num2=20;
+    let sum=num1+num2;
+    println!("The sum is:{}",sum);
+}
